277091191aadf74d87f2ebacb70fbcd3a1bfe6e2
SurenMatinyan <suren.matinyan.94@mail.ru>
2021-03-13 01:12:06 +0400

    static

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.listen(5050);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.use('/api/transaction', auth, transactionRouter);
